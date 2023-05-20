b0425e39086a1c4a401baa042e12657983000dae
Broken Deer <kxscluabcbook@outlook.com>
2023-05-20 13:37:53 +0800

    git test

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -9,9 +9,6 @@ app.use(createPinia())
 app.mount('#window')
 import { invoke, window, } from '@tauri-apps/api'
 
-// 调用命令
-// 在应用窗口中右键，打开开发者工具
-// 你会看到控制台上输出了 "Hello, World!"！
 invoke('greet', { name: 'World' })
   // `invoke` 返回的是一个 Promise
   .then((response) => console.log(response))
