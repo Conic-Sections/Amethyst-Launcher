f957d18ef5cd0d95dd13f97d475ba976b0cc3e5f
Broken-Deer <kxscluabcbook@outlook.com>
2023-12-02 16:12:31 +0800

    recover main.vue function

--- src/Main.vue
+++ src/Main.vue
@@ -4,34 +4,126 @@
       <div></div>
       <search-bar width="472px" placeholder="在 Amethyst 中搜索，或输入命令"></search-bar>
       <div class="win-btn">
-        <div class="min"><i></i></div>
-        <div class="max"><i></i></div>
-        <div class="close"><i></i></div>
+        <div class="min" @click="minimize"><i></i></div>
+        <div class="max" @click="maximize"><i></i></div>
+        <div class="close" @click="close"><i></i></div>
       </div>
     </div>
-    <div class="sidebar">
+    <div class="sidebar" data-tauri-drag-region="">
       <div class="logo">
       </div>
-      <ul class="sidebar-btns">
-        <sidebar-item title="游戏" icon="gamepad" @click="switchPage($event, 'wareHouse')"></sidebar-item>
-        <sidebar-item title="扩展" icon="puzzle-piece" @click="switchPage($event, 'community')"></sidebar-item>
-        <sidebar-item title="设置" icon="nav-5" @click="switchPage($event, 'settings')"
+      <ul class="sidebar-btns" data-tauri-drag-region>
+        <sidebar-item title="游戏" icon="gamepad" @click="changePage($event, 'wareHouse')"></sidebar-item>
+        <sidebar-item title="扩展" icon="puzzle-piece" @click="changePage($event, 'community')"></sidebar-item>
+        <sidebar-item title="设置" icon="nav-5" @click="changePage($event, 'settings')"
           style="margin-top: auto;"></sidebar-item>
-        <sidebar-item title="更多" icon="cube" @click="switchPage($event, '#more');"></sidebar-item>
+        <!-- <sidebar-item title="更多" icon="cube" @click="switchPage($event, '#more');"></sidebar-item> -->
       </ul>
     </div>
-    <main class="main">
-
+    <main class="main" style="transition: none">
+      <Transition :name="transitionName" mode="out-in">
+        <KeepAlive>
+          <component :is=" currentComponent " @back-to-home="back" @jump="jumpTo"></component>
+        </KeepAlive>
+      </Transition>
     </main>
   </div>
 </template>
 
 <script setup lang="ts">
+import { markRaw, reactive, ref, shallowRef } from 'vue';
 import SearchBar from './components/SearchBar.vue';
 import SidebarItem from './components/SidebarItem.vue';
+import { window } from '@tauri-apps/api';
+import $ from 'jquery';
+import Settings from './pages/Settings.vue';
+import Game from './pages/Game.vue';
 
-function switchPage(_a: any, _b: any) {
+// setTimeout(() => {
+//   let fontSizeError = 0
+//   const fixWebkitgtk = document.getElementById('fix-webkitgtk') as HTMLElement
+//   fixWebkitgtk.innerHTML = /* css */ `    
+//   :root {
+//     --font-size-error: ${fontSizeError}px;
+//   }`
 
+//   if ($('#font-test').outerHeight()! > 15.78) {
+//     console.log((15.78 / $('#font-test').outerHeight()!) * 15)
+//     fontSizeError = 13.5 - (15.78 / $('#font-test').outerHeight()!) * 15
+//     fixWebkitgtk.innerHTML = /* css */ `    
+//     :root {
+//       --font-size-error: ${fontSizeError}px;
+//     }`
+//   }
+// }, 48);
+
+function minimize() {
+  window.getCurrent().minimize()
+}
+function maximize() {
+  window.getCurrent().maximize()
+}
+function close() {
+  window.getCurrent().close()
+}
+
+const pages: any = reactive({
+  settings: markRaw(Settings),
+  game: markRaw(Game),
+})
+
+let transitionName = ref('entrance')
+const currentComponent = shallowRef(pages.game)
+let last: any
+function changePage(event: any, component: any) {
+  if (component === 'settings') {
+    transitionName.value = 'zoom-out'
+    hideSidebar()
+  } else {
+    showSidebar()
+    let isSettingPage = JSON.stringify(currentComponent.value) == JSON.stringify(pages.settings);
+    if (isSettingPage) {
+      transitionName.value = 'zoom-in'
+    } else {
+      transitionName.value = 'entrance'
+    }
+  }
+  last = currentComponent.value
+  if (typeof component == 'string') {
+    currentComponent.value = pages[component];
+  } else {
+    currentComponent.value = component
+  }
+}
+
+function hideSidebar() {
+  $('.main').attr('style', '')
+  $('.sidebar').addClass('sidebar-hidden')
+  $('.main').addClass('main-large')
+  setTimeout(() => {
+    $('.main').attr('style', 'transition: none')
+  }, 300);
+}
+
+function showSidebar() {
+  $('.main').attr('style', '')
+  $('.sidebar').removeClass('sidebar-hidden')
+  $('.main').removeClass('main-large')
+  setTimeout(() => {
+    $('.main').attr('style', 'transition: none')
+  }, 300)
+}
+
+function back() {
+  let isSettingPage = JSON.stringify(last) == JSON.stringify(pages.settings);
+  if (isSettingPage) {
+    changePage(null, 'wareHouse')
+    return
+  }
+  changePage(null, last)
+}
+function jumpTo(name: string) {
+  changePage(null, name)
 }
 </script>
 
@@ -65,7 +157,7 @@ function switchPage(_a: any, _b: any) {
   height: 20px;
   // border: 1px solid #f00;
   border-radius: 50%;
-  margin-left: 8px;
+  margin-left: 10px;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -145,16 +237,34 @@ function switchPage(_a: any, _b: any) {
   border-radius: 50%;
   margin-top: 16px;
   flex-shrink: 0;
+  position: absolute;
+  top: 0;
+  transition: all .3s ease;
 }
 
 .sidebar .sidebar-btns {
   width: 100%;
   height: 100%;
-  margin-top: 42px;
+  margin-top: 72px;
   display: flex;
   flex-direction: column;
   align-items: center;
   margin-bottom: 22px;
+}
+
+.sidebar>* {
+  transition: opacity .3s ease;
+}
+
+.sidebar-hidden>* {
+  opacity: 0;
+}
+
+.sidebar-hidden .logo {
+  opacity: 1;
+  position: absolute;
+  transform: scale(0.76);
+  top: -13px;
 }
 
 main.main {
@@ -169,6 +279,14 @@ main.main {
   border-right: unset;
   border-bottom-left-radius: unset;
   border-top-right-radius: unset;
+  background-color: #ffffff04;
+  transition: all .3s ease;
+}
+
+main.main-large {
+  width: 100vw;
+  border-radius: 0px;
+  border-left: none;
 }
 </style>
 <!-- <template>
---
+++ src/pages/Settings copy.vue
@@ -0,0 +1,200 @@
+<template>
+  <div class="settings" style="display: flex;padding-left: 10px;border-radius: 0px;">
+    <div class="sidebar">
+      <ul>
+        <li @click="switchComponent(item, index)" :class="[activeComponentIndex == index ? 'active' : '']"
+          v-for="(item, index) in components" :key="index"><i :class="item.icon"></i>{{ item.name }}</li>
+      </ul>
+      <li @click="$emit('backToHome')" style="margin-bottom: -9px;" class="backtoHome"><i class="arrow-left"></i>返回</li>
+    </div>
+    <div class="content" ref="content">
+      <Transition :name="transitionName" mode="out-in">
+        <KeepAlive>
+          <component :is="activeComponent"></component>
+        </KeepAlive>
+      </Transition>
+    </div>
+  </div>
+</template>
+  
+<script setup lang="ts">
+import { reactive, ref, shallowRef, type Ref, markRaw } from 'vue'
+import $ from 'jquery'
+import General from './settings/General.vue'
+import Game from './settings/Game.vue'
+import Advance from './settings/Advance.vue'
+import Appearance from './settings/Appearance.vue'
+import Download from './settings/Download.vue'
+import Accessibility from './settings/Accessibility.vue'
+import Extend from './settings/Extend.vue'
+
+const components = reactive([
+  {
+    name: '常规',
+    icon: 'house',
+    component: markRaw(General)
+  },
+  {
+    name: '游戏',
+    icon: 'gamepad',
+    component: markRaw(Game)
+  },
+  {
+    name: '高级',
+    icon: 'pro-settings',
+    component: markRaw(Advance)
+  },
+  {
+    name: '个性化',
+    icon: 'palette',
+    component: markRaw(Appearance)
+  },
+  {
+    name: '下载',
+    icon: 'download',
+    component: markRaw(Download)
+  },
+  {
+    name: '辅助功能',
+    icon: 'arrows-spin',
+    component: markRaw(Accessibility)
+  },
+  {
+    name: '扩展',
+    icon: 'cubes',
+    component: markRaw(Extend)
+  }
+])
+const activeComponent = shallowRef(General)
+let activeComponentIndex = ref(0)
+let transitionName = ref('')
+const content = ref<any>(null)
+function switchComponent(item: any, index: number) {
+  activeComponent.value = item.component
+  if (activeComponentIndex.value < index) {
+    transitionName.value = 'slide-up'
+  } else {
+    transitionName.value = 'slide-down'
+  }
+  activeComponentIndex.value = index
+}
+</script>
+  
+<style lang="less">
+.content {
+  width: 100%;
+  padding-right: 30px;
+  padding-top: 10px;
+  overflow-y: overlay;
+  overflow-x: visible;
+}
+
+.content>div {
+  width: 100%;
+}
+
+.settings {
+  padding-left: 20px !important;
+  margin-left: -60px;
+}
+
+.sidebar {
+  width: 220px;
+  font-size: calc(16px - var(--font-size-error));
+  margin-bottom: 20px;
+  margin-right: 20px;
+  transition: all 0.2s cubic-bezier(0, 0.83, 0.47, 1);
+  padding: 26px 0px 0 0px;
+  display: flex;
+  flex-direction: column;
+  flex-shrink: 0;
+  justify-content: space-between;
+}
+
+.sidebar ul {
+  padding-left: 4px;
+  padding-bottom: 4px;
+  padding-right: 4px;
+  list-style-type: none;
+  display: flex;
+  width: 100%;
+  flex-wrap: wrap;
+  max-height: calc(100vh - 196px);
+  overflow: visible overlay;
+}
+
+.sidebar li {
+  list-style: none;
+  font-size: calc(13.5px - var(--font-size-error));
+  font-weight: 500;
+  line-height: 1.3;
+  width: 100%;
+  margin: 0.1rem 0;
+  border-radius: var(--border-radius-small);
+  padding: 7px 7px;
+  display: flex;
+  align-items: center;
+  transition: all 20ms ease;
+  color: #000000cd;
+}
+
+.sidebar li:hover {
+  background-color: #ffffff45;
+}
+
+.sidebar li:active {
+  opacity: 0.55;
+}
+
+.sidebar .active {
+  background: rgba(var(--theme-color), 0.09);
+  box-shadow: 0 0 0 1px rgba(var(--theme-color), 0.1);
+  pointer-events: none;
+  color: rgba(var(--theme-color), 0.9);
+}
+
+.sidebar li::before {
+  background-color: #007bff00;
+  content: "";
+  height: 14px;
+  width: 4px;
+  position: relative;
+  left: -7px;
+  border-radius: 5px;
+  transition: all 0.15s ease;
+}
+
+.sidebar .active::before {
+  background-color: rgba(var(--theme-color), 1);
+  content: "";
+  height: 18px;
+}
+
+.sidebar i {
+  font-family: "fa-pro";
+  border-radius: 100px;
+  font-style: normal;
+  font-size: calc(16px - var(--font-size-error));
+  font-weight: 400;
+  margin-right: 7px;
+  width: 21px;
+  display: inline-flex;
+  justify-content: center;
+  align-items: center;
+  transition: all 80ms ease;
+}
+
+.sidebar>div {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+}
+
+.sidebar>div>p {
+  font-size: calc(14px - var(--font-size-error));
+  margin: 10px 2px;
+  display: inline;
+  width: fit-content;
+}
+</style>
